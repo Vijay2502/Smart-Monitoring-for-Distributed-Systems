38a6bc1ce0e262631b04a171429a481d07ee8a53
Chaitanya Naik <44868546+chets619@users.noreply.github.com>
2021-04-13 22:33:03 -0700

    Sidebar and color changes

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
+import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChartBar, faCoffee, faFolderOpen, faProjectDiagram } from '@fortawesome/free-solid-svg-icons'
 
 class Sidebar extends Component {
     state = {}
     render() {
+        let { pathname } = this.props.location;
+
         return (
             <nav>
-                <ul>
-                    <li><NavLink to="/projects">Projects</NavLink></li>
-                    <li><NavLink to="/files">Files</NavLink></li>
-                    <li><NavLink to="/projectStatus">Project Stats</NavLink></li>
-                </ul>
+                <ProSidebar className="app-sidebar">
+                    <Menu iconShape="square">
+                        <MenuItem className={pathname === "/projects" && "selected"} icon={<FontAwesomeIcon icon={faProjectDiagram} />}><NavLink to="/projects">Projects</NavLink></MenuItem>
+                        <MenuItem className={pathname === "/files" && "selected"} icon={<FontAwesomeIcon icon={faFolderOpen} />}><NavLink to="/files">Files</NavLink></MenuItem>
+                        <MenuItem className={pathname === "/projectStatus" && "selected"} icon={<FontAwesomeIcon icon={faChartBar} />}><NavLink to="/projectStatus">Project Stats</NavLink></MenuItem>
+                    </Menu>
+                </ProSidebar>
             </nav>);
     }
 }
 
-export default Sidebar;
+export default withRouter(Sidebar);
