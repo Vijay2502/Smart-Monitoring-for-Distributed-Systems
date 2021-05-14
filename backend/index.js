f1193e4a7f10b4515fce90b01e064f0ce7122ac3
Chaitanya Naik <44868546+chets619@users.noreply.github.com>
2021-05-14 16:59:43 -0700

    Deployment changes

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,11 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 const { PORT, APP_URL } = process.env;
 
 //use cors to allow cross origin resource sharing
-app.use(cors({ origin: APP_URL, credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 
 //Allow Access Control
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', APP_URL);
+    res.setHeader('Access-Control-Allow-Origin', "*");
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
